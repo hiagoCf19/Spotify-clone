7467f14f13bab1af2a4e45be9c63a45be5f1f34a
hiagoCf19 <hiagoferreira@id.uff.br>
2024-01-29 12:24:05 -0300

    organizando arquivos

---
+++ src/components/main/HeaderMain.tsx
@@ -0,0 +1,5 @@
+export const HeaderMain = () => {
+  return (
+    <div>a</div>
+  )
+}
--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -1,66 +1,9 @@
+import { HeaderMain } from "./HeaderMain"
+
 export const Main = () => {
   return (
-    <main className="flex-1 p-6 backgroundMobile rounded-[6px] h-[88vh] overflow-y-scroll">
-      <p>Conteudo principal</p>
-      <p>Conteudo principal</p><p>Conteudo principal</p>
-      <p>Conteudo principal</p><p>Conteudo principal</p>
-      <p>Conteudo principal</p><p>Conteudo principal</p>
-
-      <p>Conteudo principal</p>
-      <p>Conteudo principal</p>
-      <p>Conteudo principal</p>
-      <p>Conteudo principal</p>
-      <p>Conteudo principal</p>
-      <p>Conteudo principal</p>
-      <p>Conteudo principal</p>
-      <p>Conteudo principal</p>
-      <p>Conteudo principal</p><p>Conteudo principal</p>
-      <p>Conteudo principal</p><p>Conteudo principal</p>
-      <p>Conteudo principal</p>
-      <p>Conteudo principal</p>
-      <p>Conteudo principal</p>
-      <p>Conteudo principal</p>
-      <p>Conteudo principal</p>
-      <p>Conteudo principal</p>
-      <p>Conteudo principal</p><p>Conteudo principal</p>
-      <p>Conteudo principal</p><p>Conteudo principal</p>
-      <p>Conteudo principal</p><p>Conteudo principal</p>
-
-      <p>Conteudo principal</p>
-      <p>Conteudo principal</p>
-      <p>Conteudo principal</p>
-      <p>Conteudo principal</p>
-      <p>Conteudo principal</p>
-      <p>Conteudo principal</p>
-      <p>Conteudo principal</p>
-      <p>Conteudo principal</p>
-      <p>Conteudo principal</p><p>Conteudo principal</p>
-      <p>Conteudo principal</p><p>Conteudo principal</p>
-      <p>Conteudo principal</p>
-      <p>Conteudo principal</p>
-      <p>Conteudo principal</p>
-      <p>Conteudo principal</p>
-      <p>Conteudo principal</p>
-      <p>Conteudo principal</p>
-      <p>Conteudo principal</p><p>Conteudo principal</p>
-      <p>Conteudo principal</p><p>Conteudo principal</p>
-      <p>Conteudo principal</p><p>Conteudo principal</p>
-
-      <p>Conteudo principal</p>
-      <p>Conteudo principal</p>
-      <p>Conteudo principal</p>
-      <p>Conteudo principal</p>
-      <p>Conteudo principal</p>
-      <p>Conteudo principal</p>
-      <p>Conteudo principal</p>
-      <p>Conteudo principal</p>
-      <p>Conteudo principal</p><p>Conteudo principal</p>
-      <p>Conteudo principal</p><p>Conteudo principal</p>
-      <p>Conteudo principal</p>
-      <p>Conteudo principal</p>
-      <p>Conteudo principal</p>
-      <p>Conteudo principal</p>
-      <p>Conteudo principal</p>
+    <main className="flex-1 p-4 backgroundMobile rounded-[6px] h-[88vh] overflow-y-scroll">
+      <HeaderMain />
 
     </main>
   )
